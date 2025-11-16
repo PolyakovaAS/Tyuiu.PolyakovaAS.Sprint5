1557c30b6336b4af1b082ff6dbaceeea7c87ac23
KirillBurdov <Patrikandbooke@gmail.com>
2025-11-16 15:02:31 +0300

    Task0

--- Tyuiu.PolyakovaAS.Sprint5.Task0.V24.Lib/DataService.cs
+++ Tyuiu.PolyakovaAS.Sprint5.Task0.V24.Lib/DataService.cs
@@ -1,16 +1,14 @@
 ﻿using tyuiu.cources.programming.interfaces.Sprint5;
 namespace Tyuiu.PolyakovaAS.Sprint5.Task0.V24.Lib
 {
-    public class DataService : ISprint5Task0V25
+    public class DataService : ISprint5Task0V24
     {
         public string SaveToFileTextData(int x)
         {
-            double y = (3 * Math.Pow(x, 4) + 1) / Math.Pow(x, 3);
-            y = Math.Round(y, 3);
-
             string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask0.txt");
-            File.WriteAllText(path, y.ToString());
-
+            double z = (Math.Pow(x, 3) - 8) / (2 * Math.Pow(x, 2));
+            z = Math.Round(z, 3);
+            File.WriteAllText(path, Convert.ToString(z));
             return path;
         }
     }
