55711063be396d52c19b5978cc66856caa90bc16
PolyakovaAS <anna25.07.ap@gmail.com>
2025-11-18 12:50:46 +0300

    Add files via upload

---
+++ Tyuiu.PolyakovaAS.Sprint5.Task4.V14.Lib/DataService.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.PolyakovaAS.Sprint5.Task4.V14.Lib
+{
+    public class DataService : ISprint5Task4V14
+    {
+        public double LoadFromDataFile(string path)
+        {
+            string content;
+            using (StreamReader sr = new StreamReader(path))
+            {
+                content = sr.ReadToEnd().Trim();
+            }
+
+            double x;
+            if (!double.TryParse(content, NumberStyles.Any, CultureInfo.InvariantCulture, out x))
+            {
+                content = content.Replace('.', ',');
+            }
+
+            double res = Math.Round((Math.Sin(Math.Pow(Convert.ToDouble(x), 3)) + 2.0 / Convert.ToDouble(x)), 3);
+            return res;
+            
+        }
+    }
+}
