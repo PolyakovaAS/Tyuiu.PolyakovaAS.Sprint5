edf519bef57e1772d9202a535db60e542c9a6371
KirillBurdov <Patrikandbooke@gmail.com>
2025-11-18 12:36:48 +0300

    Task2

--- Tyuiu.PolyakovaAS.Sprint5.Task2.V1.Lib/DataService.cs
+++ Tyuiu.PolyakovaAS.Sprint5.Task2.V1.Lib/DataService.cs
@@ -1,62 +1,52 @@
 ﻿using tyuiu.cources.programming.interfaces.Sprint5;
+using System.IO;
+
 namespace Tyuiu.PolyakovaAS.Sprint5.Task2.V1.Lib
 {
     public class DataService : ISprint5Task2V1
     {
         public string SaveToFileTextData(int[,] matrix)
         {
-
-            string path = $@"{Directory.GetCurrentDirectory()}\OutPutFileTask2.csv";
-
-            FileInfo fileInfo = new FileInfo(path);
-            bool fileExits = fileInfo.Exists;
-
-            if (fileExits)
+            try
             {
-                File.Delete(path);
-            }
+                // Use a safe directory
+                string directory = Path.GetTempPath();
+                string path = Path.Combine(directory, "OutPutFileTask2.csv");
 
-            for (int i = 0; i < matrix.GetLength(0); i++)
+                // Delete existing file if it exists
+                if (File.Exists(path))
+                {
+                    File.Delete(path);
+                }
+
+                // Process matrix and build CSV content
+                var csvLines = new List<string>();
+
+                for (int i = 0; i < matrix.GetLength(0); i++)
+                {
+                    var row = new List<string>();
+                    for (int j = 0; j < matrix.GetLength(1); j++)
+                    {
+                        int value = matrix[i, j];
+                        // Set odd numbers to 0
+                        if (value % 2 != 0)
+                        {
+                            value = 0;
+                        }
+                        row.Add(value.ToString());
+                    }
+                    csvLines.Add(string.Join(";", row));
+                }
+
+                // Write to file
+                File.WriteAllLines(path, csvLines);
+
+                return path;
+            }
+            catch (Exception ex)
             {
-                for (int j = 0; j < matrix.GetLength(1); j++)
-                {
-                    if (matrix[i, j] % 2 != 0)
-                    {
-                        matrix[i, j] = 0;
-                    }
-
-                }
+                throw new Exception($"Error saving file: {ex.Message}");
             }
-
-            string str = "";
-
-            for (int i = 0; i < matrix.GetLength(0); i++)
-            {
-                for (int j = 0; j < matrix.GetLength(1); j++)
-                {
-                    if (j != matrix.GetLength(1) - 1)
-                    {
-                        str = str + matrix[i, j] + ";";
-                    }
-                    else
-                    {
-                        str = str + matrix[i, j];
-                    }
-                }
-
-                if (i != matrix.GetLength(0) - 1)
-                {
-                    File.AppendAllText(path, str + Environment.NewLine);
-                }
-                else
-                {
-                    File.AppendAllText(path, str);
-                }
-
-                str = "";
-            }
-
-            return path;
         }
     }
 }
